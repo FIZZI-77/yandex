b5ba36d660d74f14b7c6e785576a59cf317888dc
Fedor Zaprudnov <zaprudnovfedor@gmail.com>
2024-12-20 13:06:17 +0300

    first

--- src/utils/const.js
+++ src/utils/const.js
@@ -6,7 +6,7 @@ export const apiSettings = {
   serverURL: 'https://mesto.nomoreparties.co/v1/frontend-st-cohort-201',
   headers: {
     // Токен авторизации для идентификации пользователя
-    authorization: 'a05ee6fe-3b61-4627-ae5c-26cff69d358d',
+    authorization: '7e04f368-6104-4e0a-8b5e-6eb74202f7d3',
     // Указывает, что данные передаются в формате JSON
     'Content-Type': 'application/json'
   }
